a2d3976e2be46512de2d19dfc93c85785669652d
Kevin Loustau <kevin.loustau@gmail.com>
2020-11-11 16:10:06 +0100

    prepare light for shadow

--- src/3d.js
+++ src/3d.js
@@ -1,25 +1,24 @@
 var canvas = document.getElementById('renderCanvas')
 var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true })
 
-var createScene = function () {
-  var scene = new BABYLON.Scene(engine)
-  scene.clearColor = BABYLON.Color3.Black()
-  var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(-2, 5, -10), scene)
-  camera.setTarget(new BABYLON.Vector3(0, 2, 0))
-  camera.attachControl(canvas, false)
-  var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(-2, 1, 0), scene)
-  light.intensity = 1
-  return scene
-}
+var scene = new BABYLON.Scene(engine)
+scene.clearColor = BABYLON.Color3.Black()
+var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(-2, 5, -10), scene)
+camera.setTarget(new BABYLON.Vector3(0, 2, 0))
+camera.attachControl(canvas, false)
+var light1 = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(-2, 1, 0), scene)
+light1.intensity = 1
 
-var scene = createScene()
+var light2 = new BABYLON.PointLight('light2', new BABYLON.Vector3(0, 3, 0), scene)
+light2.intensity = 40
+
+var shadowGenerator = new BABYLON.ShadowGenerator(1024, light2)
+
 BABYLON.SceneLoader.ShowLoadingScreen = false
-// BABYLON.SceneLoader.AppendAsync('./assets/', 'test.glb', scene).then(function (meshes) {})
 
 BABYLON.SceneLoader.ImportMesh('', './assets/', 'test.glb', scene, function (meshes) {
   const mesh = meshes[0]
   mesh.position.x = 4
-  console.log(mesh)
   mesh.scaling = new BABYLON.Vector3(2.5, 2.5, 2.5)
 })
 
